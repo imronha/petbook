196f0ba09f8d9a6e1621b092477e828079d58755
Imron Hajiahmad <imronha@gmail.com>
2018-06-25 01:21:42 -0700

    current user profile route added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Load profile model
+const Profile = require("../../models/Profile");
+
+// Load user profile
+const Usear = require("../../models/User");
 
 // Route:  This route is a GET request to /api/profile/test
 // Desc:   Test profile route
 // Access: Public
-
 router.get("/test", (req, res) => res.json({ msg: "Profile works" }));
+
+// Route:  This route is a GET request to /api/profile
+// Desc:   Get current users profile
+// Access: Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        return res.status(404).json(errors);
+      }
+      res.json(profile);
+    });
+  }
+);
 
 module.exports = router;
