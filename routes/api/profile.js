08848669b45f4ae9d7417e1bf90855805bcc55a1
Imron Hajiahmad <imronha@gmail.com>
2018-06-29 22:13:32 -0700

    added edu and exp adding route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,7 +6,7 @@ const passport = require("passport");
 // Load Validation
 const validateProfileInput = require("../../validation/profile");
 const validateExperienceInput = require("../../validation/experience");
-// const validateEducationInput = require("../../validation/education");
+const validateEducationInput = require("../../validation/education");
 
 // Load Profile Model
 const Profile = require("../../models/Profile");
@@ -200,39 +200,39 @@ router.post(
   }
 );
 
-// // @route   POST api/profile/education
-// // @desc    Add education to profile
-// // @access  Private
-// router.post(
-//   "/education",
-//   passport.authenticate("jwt", { session: false }),
-//   (req, res) => {
-//     const { errors, isValid } = validateEducationInput(req.body);
+// Route:  This route is a POST request to /api/profile/education
+// Desc:   Add education to profile
+// Access: Private
+router.post(
+  "/education",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateEducationInput(req.body);
 
-//     // Check Validation
-//     if (!isValid) {
-//       // Return any errors with 400 status
-//       return res.status(400).json(errors);
-//     }
+    // Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
 
-//     Profile.findOne({ user: req.user.id }).then(profile => {
-//       const newEdu = {
-//         school: req.body.school,
-//         degree: req.body.degree,
-//         fieldofstudy: req.body.fieldofstudy,
-//         from: req.body.from,
-//         to: req.body.to,
-//         current: req.body.current,
-//         description: req.body.description
-//       };
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      const newEdu = {
+        school: req.body.school,
+        degree: req.body.degree,
+        fieldofstudy: req.body.fieldofstudy,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      };
 
-//       // Add to exp array
-//       profile.education.unshift(newEdu);
+      // Add to exp array
+      profile.education.unshift(newEdu);
 
-//       profile.save().then(profile => res.json(profile));
-//     });
-//   }
-// );
+      profile.save().then(profile => res.json(profile));
+    });
+  }
+);
 
 // // @route   DELETE api/profile/experience/:exp_id
 // // @desc    Delete experience from profile
