cb921d623bc82451bed73e9335ade6c78e456628
Imron Hajiahmad <imronha@gmail.com>
2018-07-03 17:36:54 -0700

    routes finished, react app created

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -38,6 +38,31 @@ router.get("/:id", (req, res) => {
     );
 });
 
+// Route:  POST request to /api/posts
+// Desc:   Create Post
+// Access: Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // If errors, send 400 with errors object
+      return res.status(400).json(errors);
+    }
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
+
 // Route:  DELETE request to /api/posts/:id
 // Desc:   Delete post by id
 // Access: Private
@@ -126,28 +151,71 @@ router.post(
   }
 );
 
-// Route:  POST request to /api/posts
-// Desc:   Create Post
+// Route:  POST request to /api/posts/comment/:id
+// Desc:   Add comment to a post
 // Access: Private
 router.post(
-  "/",
+  "/comment/:id",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    // Use same validations for posts
     const { errors, isValid } = validatePostInput(req.body);
 
     // Check Validation
     if (!isValid) {
-      // If errors, send 400 with errors object
+      // If any errors, send 400 with errors object
       return res.status(400).json(errors);
     }
-    const newPost = new Post({
-      text: req.body.text,
-      name: req.body.name,
-      avatar: req.body.avatar,
-      user: req.user.id
-    });
 
-    newPost.save().then(post => res.json(post));
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        };
+
+        // Add to comments array
+        post.comments.unshift(newComment);
+
+        // Save
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+  }
+);
+
+// Route:  DELETE request to /api/posts/comment/:id/:comment_id
+// Desc:   Delete comment from a post
+// Access: Private
+router.delete(
+  "/comment/:id/:comment_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        // Check if comment exists
+        if (
+          post.comments.filter(
+            comment => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res
+            .status(404)
+            .json({ commentdoesntexist: "Comment does not exist" });
+        }
+
+        // Get remove index
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id);
+
+        // Splice comment out of array and save
+        post.comments.splice(removeIndex, 1);
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No post found" }));
   }
 );
 
